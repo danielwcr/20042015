49ae77127f557015bda903ef8776eab13695040f
danielwcr <danielweslley@gmail.com>
2015-04-18 09:34:53 -0300

    Estrutura inicial para atender uma arquitetura DDD, separando as principais camadas: Domain, Infrastructure, Presentation, Application e Cross-Cutting.

---
+++ ProvaSiteware/ProvaSiteware.Infra.Data/Context/ProvaSitewareContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaSiteware.Infra.Data.Context
+{
+    public class ProvaSitewareContext : DbContext
+    {
+        public ProvaSitewareContext()
+            : base("ProvaSitewareDBConnection")
+        {
+
+        }
+    }
+}
---
+++ ProvaSiteware/ProvaSiteware.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProvaSiteware.MVC.MvcApplication" Language="C#" %>
