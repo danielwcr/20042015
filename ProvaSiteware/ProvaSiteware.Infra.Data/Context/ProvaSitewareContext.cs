6db69b0e56428bd791e7ae4585db34db75533983
danielwcr <danielweslley@gmail.com>
2015-04-18 10:32:32 -0300

    Optei pelo EntityFramework pela facilidade de configuração, ativei o Migrations para trabalhar com Code First e não ter que preocupar com o banco de dados.

--- ProvaSiteware/ProvaSiteware.Infra.Data/Context/ProvaSitewareContext.cs
+++ ProvaSiteware/ProvaSiteware.Infra.Data/Context/ProvaSitewareContext.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ProvaSiteware.Domain.Entities;
+using ProvaSiteware.Infra.Data.EntityConfig;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +16,25 @@ namespace ProvaSiteware.Infra.Data.Context
             : base("ProvaSitewareDBConnection")
         {
 
+        }
+
+        DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Properties().Where(p => p.Name == "Codigo").Configure(p => p.IsKey());
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties<string>().Configure(p => p.HasColumnType("varchar"));
+            modelBuilder.Properties<string>().Configure(p => p.HasMaxLength(500));
+            modelBuilder.Properties<decimal>().Configure(p => p.HasPrecision(18, 2));
+
+            modelBuilder.Configurations.Add(new ProdutoConfiguration());
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
---
+++ ProvaSiteware/ProvaSiteware.Infra.Data/EntityConfig/ProdutoConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using ProvaSiteware.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ProvaSiteware.Infra.Data.EntityConfig
+{
+    public class ProdutoConfiguration : EntityTypeConfiguration<Produto>
+    {
+        public ProdutoConfiguration()
+        {
+            HasKey(p => p.Codigo);
+            Property(p => p.Nome).IsRequired().HasMaxLength(200);
+            Property(p => p.Preco).IsRequired();
+        }
+    }
+}
